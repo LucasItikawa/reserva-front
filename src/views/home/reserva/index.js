951d0644ca53597103cd438a664fe877ee977def
LucasItikawa <lucas@hitikawa.com.br>
2024-12-12 17:00:35 -0300

    ajutes do formulario

--- src/views/home/reserva/index.js
+++ src/views/home/reserva/index.js
@@ -5,68 +5,71 @@ import { localEnum } from "../../../enum/local-enum";
 
 const RangePicker = DatePicker.RangePicker;
 const Reserva = () => {
-	const locaisOptions = localEnum.map((local) => ({
-		value: local.id,
-		label: local.local,
-	}));
+  const locaisOptions = localEnum.map((local) => ({
+    value: local.id,
+    label: local.local,
+  }));
 
-	return (
-		<Row
-			justify="center"
-			style={{
-				flexDirection: "column",
-			}}
-		>
-			<Form.Item
-				label={<span style={{ color: "#ffffff" }}>Local</span>}
-				name="local"
-				rules={[
-					{
-						required: true,
-						message: "Por favor, selecione o local!",
-					},
-				]}
-			>
-				<Select
-					placeholder="Selecione o local"
-					options={locaisOptions}
-				/>
-			</Form.Item>
+  return (
+    <Row
+      justify="center"
+      style={{
+        flexDirection: "column",
+      }}
+    >
+      <Form.Item
+        label={<span style={{ color: "#ffffff" }}>Local</span>}
+        name="local"
+        rules={[
+          {
+            required: true,
+            message: "Por favor, selecione o local!",
+          },
+        ]}
+      >
+        <Select placeholder="Selecione o local" options={locaisOptions} />
+      </Form.Item>
+      <Form.Item
+        label={<span style={{ color: "#ffffff" }}>Dia </span>}
+        name="dia"
+        rules={[
+          {
+            required: true,
+            message: "Por favor, selecione o dia!",
+          },
+        ]}
+      >
+        <DatePicker style={{ width: "100%" }} />
+      </Form.Item>
 
-			<Form.Item
-				label={<span style={{ color: "#ffffff" }}>Horário </span>}
-				name="horario"
-				rules={[
-					{
-						required: true,
-						message: "Por favor, selecione o horário!",
-					},
-				]}
-			>
-				<RangePicker
-					format="DD/MM/YYYY HH:mm"
-					showTime={{ format: "DD/MM/YYYY HH:mm" }}
-					style={{
-						width: "100%",
-					}}
-				/>
-			</Form.Item>
+      <Form.Item
+        label={<span style={{ color: "#ffffff" }}>Horário </span>}
+        name="horario"
+        rules={[
+          {
+            required: true,
+            message: "Por favor, selecione o horário!",
+          },
+        ]}
+      >
+        <DatePicker picker="time" style={{ width: "100%" }} />
+      </Form.Item>
 
-			<Form.Item>
-				<Button
-					type="primary"
-					htmlType="submit"
-					block
-					style={{
-						backgroundColor: "#4CAF50",
-						borderColor: "#4CAF50",
-					}}
-				>
-					Enviar
-				</Button>
-			</Form.Item>
-		</Row>
-	);
+      <Form.Item>
+        <Button
+          type="primary"
+          htmlType="submit"
+          block
+          style={{
+            backgroundColor: "#4CAF50",
+            borderColor: "#4CAF50",
+          }}
+        >
+          Enviar
+        </Button>
+      </Form.Item>
+    </Row>
+  );
 };
 
 export default Reserva;
