4bcd3f56ecc4e9e8b48e0de183ad08bcf0a8b117
LucasItikawa <lucas@hitikawa.com.br>
2024-12-12 23:08:18 -0300

    format date

--- src/views/home/reserva/index.js
+++ src/views/home/reserva/index.js
@@ -37,7 +37,7 @@ const Reserva = () => {
           },
         ]}
       >
-        <DatePicker style={{ width: "100%" }} />
+        <DatePicker format={"DD/MM/YYYY"} style={{ width: "100%" }} />
       </Form.Item>
 
       <Form.Item
