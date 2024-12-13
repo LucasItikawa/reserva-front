2604c3b4e083f37bfc8bd01a319307c2ddea03d0
LucasItikawa <lucas@hitikawa.com.br>
2024-12-12 23:03:20 -0300

    form reset

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -13,6 +13,7 @@ const Home = () => {
   const onFinish = async (values) => {
     try {
       await reservaService.create(values);
+      form.resetFields();
       obterDados();
       setSteps(0);
       message.success("Reserva criada com sucesso!");
